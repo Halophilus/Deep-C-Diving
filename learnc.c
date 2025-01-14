6088ae860a19ddb630f1ddece04bd9a49b488872
Halophilus <btmhenshaw@gmail.com>
2025-01-14 01:30:27 +0000

    Add C standards

--- learnc.c
+++ learnc.c
@@ -4,53 +4,103 @@
  * Ben Henshaw / CS5600 / Northeastern University
  * Spring 2025 / 1/13/2025
  *
+ * For converting gas consumption rates between imperial and metric
  */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MPG2KML 0.425144
-#define LPH2MPG 2.352
+#define MPG2KML 0.425144 /* Miles per gal. to km. per liter factor */
+#define LPH2MPG 0.02352 /* L. per 100km to MPG */
 
-void clear(){
-	while (getchar() != '\n');
+/* Function: clear
+ * -----------------------
+ *  Clears stdin input buffer
+ */
+void clear ()
+{
+	while (getchar () != '\n');
 }
 
-double mpg2kml(double mpg){
-	return mpg*MPG2KML;
+/* Function: valid_double_input
+ * -----------------------
+ *  Enforces positive double values from stdin
+ *
+ *  returns: user-inputted valid double value
+ */
+double valid_double_input(void){
+	double value;
+	while (scanf ("%lf", &value) != 1 /* While there is no matching input */
+		       	|| value < 0) /* Or the input value is negative */ 
+	{
+		printf ("Invalid input! Try again\n");
+		clear (); /* Clear the buffer */
+	}
+
+	return value;
 }
 
-double mpg2lphm(double mpg){
-	return mpg2kml(mpg)*100.0;
+
+/* Function: mpg2kml
+ * -----------------------
+ * Converts MPG to Km/L
+ *
+ * mpg:	Mileage expressed in miles per gallon
+ * 
+ * returns: the corresponding value in km/L
+ */
+double mpg2kml (double mpg)
+{
+	return mpg * MPG2KML;
 }
 
-double lph2mpg(double lph){
-	return lph*LPH2MPG;
+/* Function: mpg2lphm
+ * -----------------------
+ * Converts MPG to (100 km)/L
+ *
+ * mpg: Mileage expressed in miles per gallon
+ *
+ * returns: the corresponding value in (100 km)/L
+ */
+double mpg2lphm(double mpg)
+{
+	return mpg2kml (mpg) * 100.0;
 }
 
-int main(){
+/* Function: lph2mpg
+ * -----------------------
+ * Converts (100 km)/L to MPG
+ * 
+ * lph: Gas consumption rate in (100 km) / L
+ *
+ * returns: The corresponding value in MPG
+ */
+double lph2mpg(double lph)
+{
+	return lph * LPH2MPG;
+}
+
+/* Validates user input for the above functions */
+int main()
+{
 	double mpg;
 	double lph;
 
-	printf("Enter an MPG value\n");
-	while(scanf("%lf", &mpg) != 1 || mpg < 0){
-		printf("Invalid input! Try again\n");
-		clear();
-	}
-
-	printf("MPG to KML: %.2lf\n",mpg2kml(mpg));
-	printf("MPG to LPHM: %.2lf\n",mpg2lphm(mpg));
+	printf ("Enter an MPG value\n");
 	
-	clear();
+	mpg = valid_double_input ();
 
-	printf("Enter an LPH value\n");
+	printf ("MPG to KML: %.2lf\n", mpg2kml (mpg));
+	printf ("MPG to LPHM: %.2lf\n", mpg2lphm (mpg));
+	
+	clear ();
+
+	printf ("Enter an LPH value\n");
 	 
-	while(scanf("%lf", &lph) != 1 || lph < 0){
-		printf("Invalid input! Try again\n");
-		clear();
-	}
+	lph = valid_double_input ();
 
 
-	printf("LPH to MPH: %.2lf\n",lph2mpg(lph));
+	printf ("LPH to MPH: %.2lf\n", lph2mpg (lph));
 
 	return 0;
 }
