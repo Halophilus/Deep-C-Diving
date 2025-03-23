46e1d2744a525a6b477ce1c3112827a122ccf48f
Ben Henshaw <33325217+Halophilus@users.noreply.github.com>
2025-03-23 16:22:40 -0400

    Update message.h

--- Practicum/message.h
+++ Practicum/message.h
@@ -14,45 +14,25 @@
 #include <string.h>
 #include <time.h>
 
-// Struct:	Message
-// --------------------
-// Contains all relevant information for a message
-typedef struct Message {
-	int id;
-	time_t timestamp;
-	char sender[128];
-	char receiver[128];
-	char content[1024];
-	bool delivered;
+// Message structure definition
+typedef struct {
+    int id;
+    time_t timestamp;
+    char sender[256];
+    char receiver[256];
+    char content[1024];
+    int delivered; // 0 (undelivered) or 1 (delivered)
 } Message;
 
-// Function:	create_msg
-// -----------------------
-// Dynamically allocates a Message struct
-//
-// sender:	String of sender ID
-// receiver:	String of receiver ID
-// content:	String of message content
-//
-// returns: Message object from heap memory
-Message* create_msg(const char* sender, const char* receiver const char* content);
-
-// Function:	store_message
-// --------------------------
-// Stores copy of generated message object in a local text file
-//
-// msg:		Message object pointer
-//
-// returns: 0 on success, -1 on failure
+// Function prototypes
+Message* create_msg(const char* sender, const char* receiver, const char* content);
+Message* parse_row(char* row);
 int store_msg(const Message* msg);
-
-// Function:	retrieve_message
-// -----------------------------
-// Retrieves a message from local storage based on ID
-//
-// id:		Integer Message.id target value
-//
-// returns:	Message* object pointer
-Message* retrieve_message(int id);
+char* generate_msg_string(const Message* msg);
+Message* retrieve_msg(int id);
+int update_delivered(int id);
+int get_id(void);
+void print_msg(const Message* msg);
 
 #endif
+
